1e7fa76ae83e2e1eee920356f25c73793f3943e4
unknown <>
2016-05-15 17:06:09 +0800

    add `npm run clean`

---
+++ readme.md
@@ -0,0 +1,6 @@
+`npm run build` 编译源码到 `dist` 目录下
+
+`npm run clean` 清空`dist`目录
+
+`npm run dev` 开发模式
+
---
+++ scripts/clean.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const fsPlus = require('fs-extra');
+const config = require('./../config');
+
+if (fs.existsSync(config.DIST_PATH)) {
+    fsPlus.removeSync(config.DIST_PATH);
+    console.log('Clean Success');
+} else {
+    console.log('Don\'t need clean');
+}
